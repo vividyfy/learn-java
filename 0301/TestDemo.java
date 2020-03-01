5e448dcd387272ed68b20574de712e3e0770bbe5
vividyfy <1044292734@qq.com>
2020-03-01 20:39:23 +0800

    一些计算

---
+++ 0301/TestDemo.java
@@ -0,0 +1,18 @@
+public class TestDemo {
+	public static void main(String[] args) {
+		byte a = 130;
+		float b = 3.5;
+	}
+	public static void main2(String[] args) {
+		double x = 2.0;
+		int y = 4;
+		x /= ++y;
+		System.out.println(x);
+	}
+	public static void main1(String[] args) {
+		short a =128;
+        byte b =(byte) a;
+        System.out.println(a);
+        System.out.println(b);
+	}
+}
