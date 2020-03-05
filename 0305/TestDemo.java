42a21dd32c6e1dfef90673943070fecb07df8b4b
vividyfy <1044292734@qq.com>
2020-03-05 21:30:38 +0800

    0-100偶数和、奇数和

---
+++ 0305/TestDemo.java
@@ -0,0 +1,19 @@
+public class TestDemo {
+public static void main(String[] args) {
+		int i = 1;
+		int sum1 = 0;
+		int sum2 = 0;
+		while (i <= 100) {
+			if (i % 2 == 0) {
+			sum1 += i;
+			i++;
+			} else {
+			sum2 += i;
+			i++;
+			}	
+		}
+		System.out.println(sum1);
+		System.out.println(sum2);
+	}	
+	
+}
