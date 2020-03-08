070ba51e789cfe75b68225af2c1294a3a8435c02
vividyfy <1044292734@qq.com>
2020-03-08 11:46:03 +0800

    计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值

---
+++ 0308/TestDemo2.java
@@ -0,0 +1,12 @@
+public class TestDemo2 {
+	public static void main(String[] args) {
+		int i;
+		int j = 1;
+		double sum = 0;
+		for(i=1;i<101;i++){
+			sum += 1.0/i * j;
+			j = -j;
+		}
+		System.out.println(sum);
+	}
+}
