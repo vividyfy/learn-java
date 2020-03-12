61e94952e8261423cde0ab60b51b1203632ae86f
vividyfy <1044292734@qq.com>
2020-03-12 21:48:39 +0800

    使用递归的方法求第n项斐波那契数

---
+++ 0312/src/TestDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TestDemo {
+    public static int fib(int n) {
+        if(n==1||n==2) {
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int ret = fib(a);
+        System.out.println(ret);
+    }
+}
