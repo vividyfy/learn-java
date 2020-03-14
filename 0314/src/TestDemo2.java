f7dc55c1219c163a75319709828c10fb5828a6ca
vividyfy <1044292734@qq.com>
2020-03-14 23:41:04 +0800

    实现一个方法 avg, 以数组为参数, 求数组中所有元素的平均值

---
+++ 0314/src/TestDemo2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TestDemo2 {
+    public static int avg(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum/array.length;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[3];
+        for (int i = 0; i < 3; i++) {
+            array[i] = scan.nextInt();
+        }
+        //int[] array = {1,2,3,4,5};
+        int ret = avg(array);
+        System.out.println(ret);
+    }
+}
+
+
