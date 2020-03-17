b5b2c2828fb3ffca61ea01e9a00e7c310a34b400
vividyfy <1044292734@qq.com>
2020-03-17 11:55:47 +0800

    数字逆置

---
+++ 0317/src/TestDemo.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+//数组逆置
+public class TestDemo {
+    public static void reverse(int[] array) {
+        int left = 0;
+        int right = array.length-1;
+        while (left < right) {
+            int tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1,2,3,5,6,9,10};
+        reverse(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
