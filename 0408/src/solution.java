1906a2e437b46eefa1817d619d5d2839a4f24a03
vividyfy <1044292734@qq.com>
2020-04-08 21:36:48 +0800

    二进制链表转整数

---
+++ 0408/src/solution.java
@@ -0,0 +1,12 @@
+public class solution {
+        public int getDecimalValue(ListNode head) {
+            ListNode cur = head;
+            int a = 0;
+            while(cur!=null) {
+                a = a*2+cur.val;
+                cur = cur.next;
+            }
+            return a;
+        }
+
+}
