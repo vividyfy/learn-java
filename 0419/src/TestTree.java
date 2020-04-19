abe86867c5764546b5abafb70e6339f3d6f76b5b
vividyfy <1044292734@qq.com>
2020-04-19 22:23:08 +0800

    二叉树的遍历（前序、中序、后序）

--- 0419/src/TestTree.java
+++ 0419/src/TestTree.java
@@ -28,17 +28,37 @@ public class TestTree {
 
     //先序遍历：先访问根节点，递归访问左子树，再递归访问右子树。
     public static void preOrder(Node root) {
-        if(root==null) {
+        if (root==null) {
             return;
         }
-        System.out.print(root.val + " ");
+        //System.out.print(root.val + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
+    //中序遍历
+    public static void inOrder(Node root) {
+        if (root==null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
 
+    //后序遍历
+    public static void postOrder(Node root) {
+        if (root==null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
     public static void main(String[] args) {
         Node root = build();
         preOrder(root);
+        inOrder(root);
+        postOrder(root);
 
     }
 }
