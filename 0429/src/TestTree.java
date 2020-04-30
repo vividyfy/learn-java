349a92d83f96443f834a4b298e97f69c508e39d9
vividyfy <1044292734@qq.com>
2020-04-30 22:15:29 +0800

    检查两棵树是否相同

--- 0429/src/TestTree.java
+++ 0429/src/TestTree.java
@@ -3,7 +3,6 @@ public class TestTree {
         if (p == null && q == null) return true;
         if (q == null || p == null) return false;
         if (p.val != q.val) return false;
-        return isSameTree(p.right, q.right) &&
-                isSameTree(p.left, q.left);
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
     }
 }
