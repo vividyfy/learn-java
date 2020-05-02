54db8658c6b41273a45df3b253d948b75ed1f37a
vividyfy <1044292734@qq.com>
2020-05-02 17:32:47 +0800

    判断当前树中叶子结点的个数

---
+++ 0502/src/TestTree2.java
@@ -0,0 +1,25 @@
+public class TestTree2 {
+    
+    //1.使用静态变量
+    public static int leafSize = 0;
+    public static void size(TestDemo.Node root) {
+        if (root==null) {
+            return;
+        }
+        if (root.left==null&&root.right==null) {
+            leafSize++;
+        }
+        size(root.left);
+        size(root.right);
+    }
+    //2.不使用静态变量
+    public static int leafSize(TestDemo.Node root) {
+        if (root==null) {
+            return 0;
+        }
+        if (root.right==null&&root.left==null) {
+            return 1;
+        }
+        return leafSize(root.left) + leafSize(root.right);
+    }
+}
