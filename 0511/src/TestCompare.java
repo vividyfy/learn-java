b16fbfcd6bbb29796fae3f667da04e2a8e8f5d9d
vividyfy <1044292734@qq.com>
2020-05-11 22:26:11 +0800

    == 和 equals

---
+++ 0511/src/TestCompare.java
@@ -0,0 +1,38 @@
+
+class Card {
+    String rank;
+    String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "rank='" + rank + '\'' +
+                ", suit='" + suit + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
+
+public class TestCompare {
+    public static void main(String[] args) {
+        Card card1 = new Card("10","♥");
+        Card card2 = new Card("10","♠");
+        Card card3 = card1;
+        System.out.println(card1==card2);//比较身份，地址
+        System.out.println(card1==card3);
+//        System.out.println(card1);
+//        System.out.println(card2);
+        System.out.println(card1.equals(card2));
+        System.out.println(card1.equals(card3));
+
+    }
+}
