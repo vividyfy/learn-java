a997819a71a4eafd997387a3dd03c0fdcea8513d
vividyfy <1044292734@qq.com>
2020-05-13 22:23:27 +0800

    找出数组中只出现一次的数

---
+++ 0513/src/TestInterview.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestInterview {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            Integer count = map.get(x);
+            if (count == null) {
+                map.put(x, 1);
+            } else {
+                map.put(x, count + 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(1)) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+
+}
