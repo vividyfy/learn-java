39ef68ec9651b00974803d79bf1ba1e49da7cc0d
vividyfy <1044292734@qq.com>
2020-05-15 23:07:50 +0800

    给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。

---
+++ 0515/src/solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+public class solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.put(cur,new Node(cur.val));
+        }
+        for (Node cur = head; cur != null; cur = cur.next) {
+            Node newCur = map.get(cur);
+            newCur.next = map.get(cur.next);
+            newCur.random = map.get(cur.random);
+        }
+        return map.get(head);
+    }
+}
