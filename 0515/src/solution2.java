2d05b0d75d66b0cb492f6c5f106c2d4435a3d5c8
vividyfy <1044292734@qq.com>
2020-05-15 23:09:02 +0800

    给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。

---
+++ 0515/src/solution2.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class solution2 {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for (int i = 0; i < J.length(); i++) {
+            char t = J.charAt(i);
+            if (S.contains(t+" ")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //set方法
+    public int numJewelsInStones2(String J, String S) {
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < J.length(); i++) {
+            set.add(J.charAt(i));
+        }
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
