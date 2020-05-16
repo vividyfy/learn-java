8e7fc538b1ea8ef17daccbd62ab8613eb9eb20ea
vividyfy <1044292734@qq.com>
2020-05-16 23:42:28 +0800

    给一非空的单词列表，返回前 k 个出现次数最多的单词。

--- 0516/src/solution.java
+++ 0516/src/solution.java
@@ -1,12 +1,23 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class solution {
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map = new HashMap<>();
         for (String x : words) {
-            
+            map.put(x,map.getOrDefault(x,0)+1);
         }
+        List<String> result = new ArrayList<>(map.keySet());
+        Collections.sort(result, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int count1 = map.get(01);
+                int count2 = map.get(02);
+                if (count1==count2){
+                    return o1.compareTo(o2);
+                }
+                return count2 - count1;
+            }
+        });
+        return result.subList(0,k);
     }
 }
