dbebf7c7d59396a20c3a7598df69600c9ea2129f
vividyfy <1044292734@qq.com>
2020-05-16 23:18:00 +0800

    给一非空的单词列表，返回前 k 个出现次数最多的单词。

---
+++ 0516/src/solution.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String x : words) {
+            
+        }
+    }
+}
