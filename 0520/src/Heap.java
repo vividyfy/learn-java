27f2f0d0b3b813f1e88b1251bfdc486750be50d8
vividyfy <1044292734@qq.com>
2020-05-20 22:28:44 +0800

    堆

---
+++ 0520/src/Heap.java
@@ -0,0 +1,22 @@
+public class Heap {
+    public static void shiftDown(int[] array, int size, int index) {
+        int parent = index;
+        int child = 2 * parent + 1;
+        while (child < size) {
+            if (array[child] < array[child+1] && child + 1 < size) {
+                child = child + 1;
+            }
+            if (array[parent] < array[child]) {
+                int tmp = array[parent];
+                array[parent] = array[child];
+                array[child] = tmp;
+            } else {
+                break;
+            }
+            parent = child;
+            child = 2 * parent + 1;
+        }
+    }
+
+    
+}
