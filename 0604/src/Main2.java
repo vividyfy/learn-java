60f9d77f705c868ef3631f4128ac8b991778c556
vividyfy <1044292734@qq.com>
2020-06-04 23:37:22 +0800

    删除字符串一中出现的字符串二的字符

--- 0604/src/Main2.java
+++ 0604/src/Main2.java
@@ -8,7 +8,7 @@ public class Main2 {
             String s2 = scanner.next();
             StringBuffer s3 = new StringBuffer();
             char[] c = new char[1024];
-        for (int i = 0; i < s2.length(); i++) {
+            for (int i = 0; i < s2.length(); i++) {
             c[s2.charAt(i)] = 1;
         }
         for (int i = 0; i < s1.length(); i++) {
@@ -17,6 +17,5 @@ public class Main2 {
             }
         }
         System.out.println(s3.toString());
-
         }
-    }
+}
